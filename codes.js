77d453394edd0a2ec29f9d0f77af970cc928fcbe
Abdelbaset15 <102455367+Abdelbaset15@users.noreply.github.com>
2023-09-26 13:23:53 +0300

    Add files via upload

---
+++ codes.js
@@ -0,0 +1,13 @@
+`use strict`
+
+const anime1 = function(){
+    header.style.display='flex';
+}
+
+const anime2 = function(){
+    article.style.display='flex';
+}
+
+const anime3 = function(){
+    aside.style.display='flex';
+}
